24bab314e49af540489d7f63251d7a2d8d569476
Manuel Florez <manuelflorezw@gmail.com>
2022-03-18 12:07:05 -0500

    first commit

---
+++ src/test/java/co/edu/ufps/iot/MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
